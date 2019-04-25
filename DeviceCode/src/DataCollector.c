09f3b1cd05088ce8719bf42b07a60db6c7a35fc7
Rokas <rokasgz21@gmail.com>
2019-04-25 08:36:11 +0200

    Add CO2, Add Movement, Add initial Temp/Humid, Break Lora :(

--- DeviceCode/src/DataCollector.c
+++ DeviceCode/src/DataCollector.c
@@ -7,6 +7,10 @@
 #include <EventReactor.h>
 #include <stdlib.h>
 #include <time.h>
+#include <hcSr501.h>
+#include <mh_z19.h>
+#include <serial.h>
+#include <hih8120.h>
 
 //FIXME Define final ports
 
@@ -21,11 +25,12 @@ static TaskHandle_t CO2Handle=NULL;
 static TaskHandle_t TempHandle=NULL;
 static TaskHandle_t SoundHandle=NULL;
 static TaskHandle_t MovementHandle=NULL;
+static hcSr501_p hcSr501Inst=NULL;
 
 static TimerHandle_t sensorTimer;
 
-
 void sensorTimerCallback(TimerHandle_t pxTimer);
+void co2_callback(uint16_t co2_ppm);
 
 void initializeDataCollector(sensor_data_t* sensorData, SemaphoreHandle_t* semaphoreHandle){
     sensorDataPrivate=sensorData;
@@ -40,12 +45,19 @@ void initializeDataCollector(sensor_data_t* sensorData, SemaphoreHandle_t* semap
     sensorTimer=xTimerCreate("SENSOR_TIMER",pdMS_TO_TICKS(1000),pdTRUE,(void*)0,sensorTimerCallback);
     xTimerStart(sensorTimer,0);
 
-    //FIXME REMOVE IN FINAL
-    srand(time(NULL));
-
-    //TODO Replace me with CO2 Sensor initiliazer
-    //TODO replace me with Temp Sensor initializer
-    //TODO replace me with movement sensor initializer
+    //--------------------------------------------- MOVEMENT SENSOR SETUP --------------------------------------------------//
+    hcSr501Inst = hcSr501Create(&PORTE, PE5);
+    if ( NULL != hcSr501Inst )
+    {
+        printf("DRIVER_INITIALIZED");
+    }
+    //--------------------------------------------- CO_2 SENSOR SETUP --------------------------------------------------//
+    mh_z19_create(ser_USART3, co2_callback);
+    //--------------------------------------------- TEMP_HUMIDITY SENSOR SETUP --------------------------------------------------//
+    if ( HIH8120_OK == hih8120Create() )
+    {
+        printf("TEMP_HUMIDITY_INITIALIZED /n");
+    }
 
 }
 
@@ -57,9 +69,14 @@ void gatherCO2(){
         xSemaphoreTake(*semaphore,SENSOR_TIMER*60);
         //FIXME IMPLEMENT ME
         printf("CO2 TASK %d \n",xLastWakeTimeCO2);
-        sensorDataPrivate->CO2+=rand()%1000;
+        mh_z19_return_code_t rc;
+        rc = mh_z19_take_meassuring();
+        if (rc != 0)
+        {
+            printf("CO2_SENSOR_ERROR\n");
+        }
         xSemaphoreGive(*semaphore);
-        vTaskDelayUntil(&xLastWakeTimeCO2,SENSOR_TIMER*60);
+        vTaskDelayUntil(&xLastWakeTimeCO2,SENSOR_TIMER*20);
     }
 
 #pragma clang diagnostic pop
@@ -71,11 +88,26 @@ void gatherTempAndHumidity(){
     TickType_t xLastWakeTimeTemp=xTaskGetTickCount();
     while(1){
         xSemaphoreTake(*semaphore,SENSOR_TIMER*60);
-        printf("SENSOR_WAKE_UP\n");
+
+        if ( HIH8120_OK != hih8120Wakeup() )
+        {
+            printf("WAKE_UP_FAILED\n");
+        }
+
         vTaskDelay(pdMS_TO_TICKS(100));
-        sensorDataPrivate->temperature+=rand()%25;
-        sensorDataPrivate->temperature+=rand()%159;
-        printf("SENSOR_WOKE_UP \n");
+        if ( HIH8120_OK !=  hih8120Meassure() )
+        {
+            printf("MEASURE_FAILED\n");
+        }
+
+        float humidity = 0.0;
+        float temperature = 0.0;
+        humidity = hih8120GetHumidity();
+        temperature = hih8120GetTemperature();
+        uint8_t *array;
+        array = (uint8_t*)(&temperature);
+        printf("%i:%i:%i:%i \n",array[0],array[1],array[2],array[3]);
+        printf("%.2f:TEMP\n",temperature);
         xSemaphoreGive(*semaphore);
         vTaskDelayUntil(&xLastWakeTimeTemp,SENSOR_TIMER*60);
     }
@@ -108,15 +140,16 @@ void monitorMovement(){
         xSemaphoreTake(*semaphore,SENSOR_TIMER);
         printf("MOVEMENT TASK \n");
         //FIXME Remove the following 3 lines as it's simply for testing purposes
-        int random=rand()%10;
-        if(random>5){
-            sensorDataPrivate->movement++;
-        }
-        if(PROXIMITY_PIN==1){
+        if ( hcSr501IsDetecting(hcSr501Inst) )
+        {
+            sensorDataPrivate->movement+=1;
+            printf("MOVEMENT_DETECTED\n");
             vTaskResume(eventReactorTask);
             xSemaphoreGive(*semaphore);
             vTaskDelayUntil(&xLastWakeTimeMovement,SENSOR_TIMER*10);
-        } else{
+        }
+        else
+        {
             xSemaphoreGive(*semaphore);
             vTaskDelayUntil(&xLastWakeTimeMovement,SENSOR_TIMER*1);
         }
@@ -126,7 +159,8 @@ void monitorMovement(){
 }
 
 void co2_callback(uint16_t co2_ppm){
-    sensorDataPrivate->CO2=co2_ppm;
+    sensorDataPrivate->CO2=sensorDataPrivate->CO2+co2_ppm;
+    printf("%i CO2_DATA \n",sensorDataPrivate->CO2);
 }
 
 void sensorTimerCallback(TimerHandle_t pxTimer){
