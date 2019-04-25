dd14046ee244cd5f1dc5d6c4fe42b4a88c395812
Michi <11michi11@gmail.com>
2019-04-25 10:16:07 +0200

    Prepared websocket client

--- BridgeApp/src/main/java/embedded/BridgeApp/application/HandleDevice.java
+++ BridgeApp/src/main/java/embedded/BridgeApp/application/HandleDevice.java
@@ -1,27 +1,54 @@
 package embedded.BridgeApp.application;
 
 import embedded.BridgeApp.persistance.MongoRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 
-public class HandleDevice implements Runnable{
+import java.lang.reflect.Type;
+
+public class HandleDevice extends StompSessionHandlerAdapter {
+
+    @Getter
     private int deviceId;
+
     @Autowired
     private MongoRepository mongoRepository;
+    private String downlinkTopic = "/topic/messages";
+    private String uplinkTopic = "/app/chat";
+    private StompSession session;
 
-    public HandleDevice(int deviceId) {
-        this.deviceId = deviceId;
-    }
-
-    public byte[] receiveTransmision() {
-        return null;
-    }
-
-    public void sendCommand(byte[] data) {
-
+    /*
+     *@param data Json as String with prepared payload for uplink msg
+     */
+    public void sendCommand(String data) {
+        session.send(uplinkTopic, data);
     }
 
     @Override
-    public void run() {
-
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+        this.session = session;
+        session.subscribe(downlinkTopic, this);
+        session.send(uplinkTopic, "Sample msg");
     }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        Message msg = (Message) payload;
+       // System.out.println("Received : " + msg.getText() + " from : " + msg.getFrom());
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return Message.class;
+    }
+
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+        System.out.println("Got an exception" + exception);
+    }
+
 }
