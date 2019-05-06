305605ffd01587c9e129032138559ed5ed82ca75
Rokas <rokasgz21@gmail.com>
2019-05-06 21:36:57 +0200

    In theory fix servo delay

--- DeviceCode/src/EventReactor.c
+++ DeviceCode/src/EventReactor.c
@@ -1,52 +1,20 @@
 
-#include "EventReactor.h"
-#define BUZZER_PORT (PB7)
-#define LIGHT_PORT (PB6)
+#include <EventReactor.h>
+#include <servo.h>
 
-static const int LIGHT_DURATION_S=(954/portTICK_PERIOD_MS)*10;
-static const int BUZZER_DURATION_S=(954/portTICK_PERIOD_MS)*5;
-TaskHandle_t eventReactorTask=NULL;
 static preferences_t* privatePreferences;
 
 void initializeEventReactor(preferences_t* preferences){
 	initialize_servo_motor();
 	privatePreferences=preferences;
-    xTaskCreate(toggleLights,"LIGHT_TOGGLE",configMINIMAL_STACK_SIZE,NULL,2,&eventReactorTask);
-    vTaskSuspend(eventReactorTask);
-    DDRB |= _BV(LIGHT_PORT);
-    DDRB |= _BV(BUZZER_PORT);
 }
 
-//TODO MERGE toggleLights with toggleBuzzer due to the fact that they both do the same thing, just take different out port
+
 void toggleLights(){
-	#pragma clang diagnostic push
-	#pragma clang diagnostic ignored "-Wmissing-noreturn"
-	//FIXME IMPLEMENT ME
-	printf("PRE-DELAY-TOGGLE \n");
-	servo_move_to_90();
-	printf("LIGHT TOGGLE 1\n");
-	vTaskDelay((954/portTICK_PERIOD_MS)*10);
-	servo_move_to_0();
-	printf("LIGHT TOGGLE 2\n");
-	#pragma clang diagnostic pop
+	printf("TOGGLE_EXECUTE \n");
+ 	servo_move_to_90();
+ 	servo_move_to_0();
 }
 
-void toggleBuzzer(){
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-    while(1){
-        //FIXME IMPLEMENT ME
-        printf("PRE-DELAY-TOGGLE \n");
-        if(privatePreferences->buzzerPreference){
-            vTaskDelay(BUZZER_DURATION_S);
-            vTaskSuspend(eventReactorTask);
-        }
-        else{
-            vTaskSuspend(eventReactorTask);
-        }
-        printf("POST-DELAY-TOGGLE \n");
-    }
-#pragma clang diagnostic pop
-}
 
 
--- DeviceCode/src/MovementHandler.c
+++ DeviceCode/src/MovementHandler.c
@@ -7,11 +7,12 @@
 #include <EventReactor.h>
 #include "MovementHandler.h"
 
+#define LIGHT_TURN_ON_TIMER_IN_SECONDS 20
+
 static TaskHandle_t MovementHandle=NULL;
 static hcSr501_p hcSr501Inst=NULL;
 static uint16_t* private_movement_pointer;
 static SemaphoreHandle_t* private_semaphore;
-int const MOVEMENT_SENSOR_TIMER=954/portTICK_PERIOD_MS;
 
 
 void movement_handler_initialize(uint16_t* movement_pointer, SemaphoreHandle_t *semaphoreHandle){
@@ -21,7 +22,7 @@ void movement_handler_initialize(uint16_t* movement_pointer, SemaphoreHandle_t *
         hcSr501Inst = hcSr501Create(&PORTE, PE5);
         if ( NULL != hcSr501Inst )
         {
-            printf("DRIVER_INITIALIZED \n");
+            printf("MOVEMENT_DRIVER_INITIALIZED \n");
         }
         //--------------------------------------------- MOVEMENT SENSOR SETUP --------------------------------------------------//
         xTaskCreate(movement_task, "MOVEMENT_TASK", configMINIMAL_STACK_SIZE, NULL, MOVEMENT_SENSOR_TASK_PRIORITY,&MovementHandle);
@@ -30,23 +31,24 @@ void movement_handler_initialize(uint16_t* movement_pointer, SemaphoreHandle_t *
 void movement_task(){
 	#pragma clang diagnostic push
 	#pragma clang diagnostic ignored "-Wmissing-noreturn"
-
 	TickType_t xLastWakeTimeMovement=xTaskGetTickCount();
 	while(1){
-		xSemaphoreTake(*private_semaphore,MOVEMENT_SENSOR_TIMER);
+		xSemaphoreTake(*private_semaphore,ONE_SECOND_SENSOR_TIMER);
 		printf("MOVEMENT TASK \n");
 		if ( hcSr501IsDetecting(hcSr501Inst) )
 		{
 			*private_movement_pointer+=1;
 			printf("MOVEMENT_DETECTED\n");
-			toggleLights();
-			vTaskDelayUntil(&xLastWakeTimeMovement,MOVEMENT_SENSOR_TIMER*10);
 			xSemaphoreGive(*private_semaphore);
+			toggleLights();
+            vTaskDelay(ONE_SECOND_SENSOR_TIMER*LIGHT_TURN_ON_TIMER_IN_SECONDS);
+            toggleLights();
+			vTaskDelayUntil(&xLastWakeTimeMovement,ONE_SECOND_SENSOR_TIMER*LIGHT_TURN_ON_TIMER_IN_SECONDS);
 		}
 		else
 		{
 			xSemaphoreGive(*private_semaphore);
-			vTaskDelayUntil(&xLastWakeTimeMovement,MOVEMENT_SENSOR_TIMER*1);
+			vTaskDelayUntil(&xLastWakeTimeMovement,ONE_SECOND_SENSOR_TIMER*1);
 		}
 	}
 	#pragma clang diagnostic pop
