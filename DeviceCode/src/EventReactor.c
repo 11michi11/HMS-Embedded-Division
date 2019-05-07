52836bc3788ec74638e6914ba70f876829ed5522
gundarsv <267009@viauc.dk>
2019-05-07 09:23:38 +0200

    Fix delay between toggleLights

--- DeviceCode/src/EventReactor.c
+++ DeviceCode/src/EventReactor.c
@@ -1,6 +1,7 @@
 
 #include <EventReactor.h>
 #include <servo.h>
+#include "DataRepository.h"
 
 static preferences_t* privatePreferences;
 
@@ -12,7 +13,9 @@ void initializeEventReactor(preferences_t* preferences){
 
 void toggleLights(){
 	printf("TOGGLE_EXECUTE \n");
+	
  	servo_move_to_90();
+	 vTaskDelay(ONE_SECOND_SENSOR_TIMER);
  	servo_move_to_0();
 }
 
